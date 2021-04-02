53022b5acec0390106301f1c73765313c0caccfa
DreamChaser-luzeyu <yydluzeyu@gmail.com>
2021-04-02 20:24:10 +0800

    version 0.1.4

--- ELinkFinal/main.cpp
+++ ELinkFinal/main.cpp
@@ -1,20 +1,11 @@
-#include "mainwindow.h"
-#include <QFile>
+#include "widget.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QString qss;
-    QFile qssFile(":/BinDreamUI.qss");
-    qssFile.open(QFile::ReadOnly);
-    if(qssFile.isOpen()){
-        qss = QLatin1String(qssFile.readAll());
-        qApp->setStyleSheet(qss);
-        qssFile.close();
-    }
-
-    MainWindow w;
+    Widget w;
     w.show();
     return a.exec();
 }
--- ELinkFinal/scrcpy/scrcpy-noconsole.vbs
+++
@@ -1,1 +0,0 @@
-CreateObject("Wscript.Shell").Run "cmd /c scrcpy.exe", 0, false
